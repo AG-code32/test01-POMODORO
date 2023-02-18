0a9a40dc89fca93bc6159189772578a73a505c2f
AG-code32 <i.gerson@hotmail.com>
2023-02-18 18:09:11 -0500

    cambios: js, html

--- javascript/main.js
+++ javascript/main.js
@@ -7,6 +7,11 @@ let current = null;
 const bAdd = document.querySelector('#bAdd');
 const itTask = document.querySelector('#itTask');
 const form = document.querySelector('#form');
+const taskName = document.querySelector('#time #taskName');
+
+renderTime();
+renderTasks();
+
 
 console.log(itTask.value);
 
@@ -40,11 +45,99 @@ function renderTasks() {
     const html = tasks.map(task => {
         return `
             <div class="task">
-                <div class="completed">${task.completed ? `<span class="done">Done</span>`:`<button class="start-button" data-id="${task.id}"></button>`}</div>
+                <div class="completed">${task.completed ? `<span class="done">Done</span>`:`<button class="start-button" data-id="${task.id}">Start</button>`}</div>
                 <div class="title">${task.title}</div>
             </div>
         `;
     });
-    console.log(html);
+    // console.log(html);
+
+    const tasksContainer = document.querySelector('#tasks');
+    tasksContainer.innerHTML = html.join('');
+    console.log(tasksContainer);
+
+    const startButtons = document.querySelectorAll('.task .start-button');
+    console.log(startButtons);
+
+    startButtons.forEach(button => {
+        button.addEventListener('click', e => {
+            if (!timer){
+                const id = button.getAttribute('data-id')
+                console.log(id);
+
+                startButtonHandler(id);
+                button.textContent = 'In progress ...'
+            }
+        });
+    });
+
+}
+
+
+function startButtonHandler(id){
+    time = 5;
+    current = id;
+    const taskIndex = tasks.findIndex(task => task.id === id);
+    console.log(taskIndex);
+
+    taskName.textContent = tasks[taskIndex].title;
+
+    timer = setInterval(() => {
+        timerHandler(id);
+    }, 1000);
+};
+
+function timerHandler(id){
+    time--;
+    renderTime();
+    if (time === 0) {
+        clearInterval(timer);
+        markCompleted(id);
+        timer = null;
+        // current = null;
+        // taskName.textContent = '';
+        renderTasks();
+        startBreak();
+    }
+};
+
+function startBreak() {
+    time = 3;
+    taskName.textContent = 'Break';
+    timerBreak = setInterval(() => {
+        timerBreakHandler();
+    }, 1000);
+}
+
+function timerBreakHandler() {
+    time--;
+    renderTime();
+    if (time === 0) {
+        clearInterval(timerBreak);
+        current = null;
+        timerBreak = null;
+        taskName.textContent = '';
+        renderTasks();
+
+    }
+}
+
+function renderTime(){
+    const timeDiv = document.querySelector('#time #value');
+    console.log(timeDiv);
+
+    const minutes = parseInt(time / 60);
+    console.log(minutes);
+
+    const seconds = parseInt(time % 60);
+    // console.log(seconds);
+
+    timeDiv.textContent = `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+}
+
+function markCompleted(id) {
+    const taskIndex = tasks.findIndex(task => task.id === id);
+    tasks[taskIndex].completed = true;
+    console.log(tasks[taskIndex]);
 
 }
